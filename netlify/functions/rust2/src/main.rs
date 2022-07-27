ad10d9ae52d1336daabcf696df3d6afc0becc793
Daniel Tschinder <231804+danez@users.noreply.github.com>
2022-07-27 11:43:26 +0200

    proper function

--- netlify/functions/rust2/src/main.rs
+++ netlify/functions/rust2/src/main.rs
@@ -1,5 +1,31 @@
 use ring::rand::SystemRandom;
+use aws_lambda_events::event::apigw::{ApiGatewayProxyRequest, ApiGatewayProxyResponse};
+use aws_lambda_events::encodings::Body;
+use http::header::HeaderMap;
+use lambda_runtime::{handler_fn, Context, Error};
+use log::LevelFilter;
+use simple_logger::SimpleLogger;
 
-fn main() -> () {
-  let rand = SystemRandom::new();
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    SimpleLogger::new().with_level(LevelFilter::Info).init().unwrap();
+    let rand = SystemRandom::new();
+
+    let func = handler_fn(my_handler);
+    lambda_runtime::run(func).await?;
+    Ok(())
+}
+
+pub(crate) async fn my_handler(event: ApiGatewayProxyRequest, _ctx: Context) -> Result<ApiGatewayProxyResponse, Error> {
+    let path = event.path.unwrap();
+
+    let resp = ApiGatewayProxyResponse {
+        status_code: 200,
+        headers: HeaderMap::new(),
+        multi_value_headers: HeaderMap::new(),
+        body: Some(Body::Text("Hello world".to_owned())),
+        is_base64_encoded: Some(false),
+    };
+
+    Ok(resp)
 }
